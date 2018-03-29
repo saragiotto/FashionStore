caf8bca5f80bcf9daccae06d3bfba83025a981d7
lsaragiottobrq <leonardoaugustonsaragiotto@correio.itau.com.br>
2018-03-29 17:09:01 -0300

    New response object Model and add git ignore file

---
+++ FashionStore/Model/ProductResponse.swift
@@ -0,0 +1,9 @@
+//
+//  ProductResponse.swift
+//  FashionStore
+//
+//  Created by Leonardo Augusto N Saragiotto on 29/03/18.
+//  Copyright © 2018 Leonardo Augusto N Saragiotto. All rights reserved.
+//
+
+import Foundation
