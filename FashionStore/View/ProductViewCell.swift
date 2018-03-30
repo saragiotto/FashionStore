9be92f223abee1a01e2f2b64caf6767c74654c91
Leonardo Saragiotto <leonardo.saragiotto@gmail.com>
2018-03-30 11:52:42 -0300

    View Controller and View Cell created

---
+++ FashionStore/View/ProductViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductViewCell.swift
+//  FashionStore
+//
+//  Created by Leonardo Saragiotto on 3/30/18.
+//  Copyright © 2018 Leonardo Augusto N Saragiotto. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
